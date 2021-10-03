9ae9941187e04975dd614be3c0ec265588165b21
Leandro Alves <leandrojoapi@gmail.com>
2021-10-03 02:39:15 -0300

    Equipe GET - Execução da query de listagem

--- backend/src/main/java/br/com/stefanini/dao/EquipeDao.java
+++ backend/src/main/java/br/com/stefanini/dao/EquipeDao.java
@@ -1,12 +1,15 @@
 package br.com.stefanini.dao;
 
 import br.com.stefanini.models.Equipe;
+import br.com.stefanini.models.Pessoa;
 
 import javax.enterprise.context.RequestScoped;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
+import javax.persistence.TypedQuery;
 import javax.transaction.Transactional;
+import java.util.List;
 
 /**
  * @author leandro
@@ -30,5 +33,13 @@ public class EquipeDao {
         query.setParameter("nome",equipe.getNome());
 
         return query.executeUpdate();
+    }
+
+    public List<Equipe> listar()  {
+        String nameQuery = "LISTAR_EQUIPE";
+
+        TypedQuery<Equipe> query = em
+                .createNamedQuery(nameQuery, Equipe.class);
+        return query.getResultList();
     }
 }
