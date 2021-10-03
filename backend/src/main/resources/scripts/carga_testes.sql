b60bcee5fdb34cb017ffc70ab3ec51faa68dcf12
Leandro Alves <leandrojoapi@gmail.com>
2021-10-03 02:07:49 -0300

    Equipe - Criação da tabela Equipe e chave estrangeira

--- backend/src/main/resources/scripts/carga_testes.sql
+++ backend/src/main/resources/scripts/carga_testes.sql
@@ -1,5 +1,20 @@
 CREATE SCHEMA IF NOT EXISTS H2DB AUTHORIZATION sa;
 
+create table H2DB.equipe
+(
+    id int not null,
+    nome varchar(255) not null,
+);
+
+create unique index equipe_id_uindex
+    on H2DB.equipe (id);
+
+alter table H2DB.equipe
+    add constraint equipe_pk
+        primary key (id);
+
+alter table H2DB.equipe modify id int auto_increment;
+
 create table H2DB.pessoa
 (
     id int not null,
@@ -25,3 +40,10 @@ alter table H2DB.pessoa
         primary key (id);
 
 alter table H2DB.pessoa modify id int auto_increment;
+
+alter table H2DB.pessoa
+    add foreign key (codigo_equipe)
+        references H2DB.equipe(id);
+
+
+
